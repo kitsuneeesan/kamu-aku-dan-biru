438381269be8ae735e153ef457ec11b407d2f2e8
kitsuneeesan <rananda198@gmail.com>
2022-12-05 16:53:43 +0700

    dynamic import

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,48 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const type = urlParams.get('type');
-console.log(type);
+async function getMessages(type) {
+    const { messages } = await import(`./text/${type}.js`);
+    return messages;
+}
 
 const fireworks = ['⠄', '⡢', '⢑', '⠈', '⠀', '⢀', '⣠', '⣤', '⡶', '⠞', '⠋', '⠁', '⠀', '⠈', '⠙', '⠳', '⣆', '⡀', '⠀', '⠆', '⡷', '⣹', '⢈', '⠀', '⠐', '⠪', '⢅', '⡀', '⠀'];
-var fire_index = 0;
+let fire_index = 0;
 
 function startTime() {
     document.title = fireworks[fire_index];
-    setTimeout(startTime, 300);
+    setTimeout(startTime, 200);
     fire_index++;
     if (fire_index == fireworks.length) {
         fire_index = 0
     }
+}
+
+async function getType(type) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('type');
+}
+
+function typewriter(messages) {
+    messages.then((messages) => {
+        let index = 0;
+        const random_index = Math.floor(Math.random() * messages.length) + 0,
+            speed = 150;
+        function writer() {
+            const text = messages[random_index];
+            if (index < text.length) {
+                document.getElementById("message").innerHTML += text.charAt(index);
+                index++;
+                setTimeout(writer, speed);
+            }
+        }
+        writer();
+    });
+}
+
+window.onload = async () => {
+    startTime();
+    await getType().then((type) => {
+        var title = document.getElementById('title');
+        title.innerHTML = atob(type).toUpperCase();
+        typewriter(getMessages(type));
+    });
 }
